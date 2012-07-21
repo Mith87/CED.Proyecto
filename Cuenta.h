5d87dcbc24fdd1ff5b94df93344533ced01cb5ff
Mith <jnaranjo_87@hotmail.com>
2012-07-21 15:37:12 -0600

    fix Cuenta.h

--- Cuenta.h
+++ Cuenta.h
@@ -2,26 +2,26 @@
 #define CUENTA_H
 
 class Cuenta {
-	public:
-		Cuenta(string num, string tipo, 
-			   string nombre, float saldo = 0);
-		~Cuenta();
+public:
+	Cuenta(string num, string tipo, 
+		   string nombre, float saldo = 0);
+	~Cuenta();
 		
-		int crearCuenta(string nombre, string tipo, 
+	int crearCuenta(string nombre, string tipo, 
 						float saldo = 0);
 		
-		float getSaldo();
-		string getTipo();
-		string getNombreCliente();
-		string getNumero();
+	float getSaldo();
+	string getTipo();
+	string getNombreCliente();
+	string getNumero();
 		
-		void setSaldo(float saldo);
+	void setSaldo(float saldo);
 
-    private:
-		string num; 
-		string tipo;
-		string nombreCliente;
-	    float saldo;
-	};
+private:
+	string num; 
+	string tipo;
+	string nombreCliente;
+	float saldo;
+};
 
 #endif
