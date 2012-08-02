26ededb7c287c28813083334aebe4885f36c64a8
Mith <jnaranjo_87@hotmail.com>
2012-08-02 12:58:15 -0600

    Agregada funcion impresion in-orden en Arbin.cpp

--- Arbin.h
+++ Arbin.h
@@ -14,6 +14,7 @@ class Arbin
         Nodo *buscarNodo(Nodo *, std::string);
         void insertarNodo(Cuenta *);
         void borrarNodoCascada(Nodo *);
+        void ImprimirInOrden(void (*impr)(Nodo *) , Nodo *nodo=NULL);
 
     private:
         Nodo *raiz;
